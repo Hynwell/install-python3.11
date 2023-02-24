a5bd254765dfa9f4824ee15a0f30bbd653a1aea0
Hynwell <hynwellG@gmail.com>
2023-02-24 13:50:30 +0300

    first commit

---
+++ install-python3.11.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+read -p "Уставить pytnon3.11 (y/n?): " inpy
+if [[ "$inpy" == "y" ]]; then
+
+    echo "---Linux UPDATE---"    
+    sudo apt update && sudo apt upgrade -y
+
+    echo "---Install Python3.11---"
+
+    # Установка необходимого софта, для сборки python
+    softwares_list_for_python=(build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev pkg-config)
+    for software_list_for_python in ${softwares_list_for_python[@]}
+        do
+            sudo apt install $software_list_for_python -y
+        done
+    
+    # Скачиваем файлы python3.11
+    curl -o ~/download/Python-3.11.1.tgz https://www.python.org/ftp/python/3.11.1/Python-3.11.1.tgz
+    tar -xf ~/download/Python-3.11.1.tgz -C ~/download/
+
+    # Сборка Python
+    cd ~/download/Python-3.11.1
+    ./configure --enable-optimizations
+    make -j $(nproc)
+    sudo make altinstall
+
+    # Удаление исходников
+    cd ~/
+    sudo rm -rf ~/download/Python-3.11.1.tgz ~/download/Python-3.11.1
+
+    echo "---THE END---"
+    python3.11 --version
+    
+elif [[ "$inpy" == "n" ]]; then
+    echo "Отмена установки Python"
+else
+  echo "Выбранно не верное значение -> Отмена установки Python"
+fi
